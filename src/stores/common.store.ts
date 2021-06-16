19697327e9f0c9c9167d4b971841715072c59243
joe-sky <hitomi_doax@sina.com>
2021-06-16 19:42:50 +0800

    refactor: :art: rename models folder to types

--- src/stores/common.store.ts
+++ src/stores/common.store.ts
@@ -1,5 +1,5 @@
 import { makeAutoObservable, observable, computed, toJS } from 'mobx';
-import { UserInfo } from '../models';
+import { UserInfo } from '../types';
 
 export default class CommonStore {
   userInfo: UserInfo = { pin: 'joe', name: 'Joe_Sky' };
